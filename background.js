1a35a6515e06ead17774fc6f23aefe2ee4b813a1
J.C. Nelson <32139633+xC0000005@users.noreply.github.com>
2021-02-13 20:21:50 -0800

    Code cleanup.

--- background.js
+++ background.js
@@ -1,31 +1,16 @@
 // Background scripts for the Ancestry extension
-// This script is responsible for collection and collating data.
-// The UI is responsible for displaying it.
-chrome.runtime.onMessage.addListener(function(message, callback) {
-    if (message.data == "setMatchList") {
-      chrome.alarms.create({delayInMinutes: 5})
-    } else if (message.data == "addSharedMatches") {
-      chrome.tabs.executeScript({file: 'logic.js'});
-    } else if (message.data == "clearSharedMatches") {
-      chrome.tabs.executeScript(
-          {code: 'document.body.style.backgroundColor="orange"'});
-    };
 
-    return true;
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { pathContains: 'match-list' },
+          })
+        ],
+        actions: [ new chrome.declarativeContent.ShowPageAction() ]
+      }
+    ]);
   });
-
-  // When the extension is installed or upgraded ...
-  chrome.runtime.onInstalled.addListener(function() {
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-      chrome.declarativeContent.onPageChanged.addRules([
-        {
-          conditions: [
-            new chrome.declarativeContent.PageStateMatcher({
-              pageUrl: { pathContains: 'match-list' },
-            })
-          ],
-          actions: [ new chrome.declarativeContent.ShowPageAction() ]
-        }
-      ]);
-    });
-  });
+});
--- contentScript.js
+++
@@ -1,4 +0,0 @@
-// this is the code which will be injected into a given page...
-
-(function() {
-})();
